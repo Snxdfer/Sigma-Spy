d41b6c34531feaa66e813c10629b0ea2d2850f1a
00Fazee <121313122+Snxdfer@users.noreply.github.com>
2026-01-07 16:21:04 -0300

    Update Main.lua

--- src/Main.lua
+++ src/Main.lua
@@ -3,8 +3,8 @@ local Configuration = {
 	UseWorkspace = false, 
 	NoActors = false,
 	FolderName = "Sigma Spy",
-	RepoUrl = "https://raw.githubusercontent.com/0xfinder/Sigma-Spy/refs/heads/main",
-	ParserUrl = "https://raw.githubusercontent.com/depthso/Roblox-parser/refs/heads/main/dist/Main.luau"
+	RepoUrl = "https://raw.githubusercontent.com/Snxdfer/Sigma-Spy/refs/heads/main",
+	ParserUrl = "https://raw.githubusercontent.com/Snxdfer/Sigma-Spy/refs/heads/main/parser.lua"
 }
 
 --// Load overwrites
